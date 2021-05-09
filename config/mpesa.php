18e719186e01f2f6bc4c726ab57eec3deef0f8c6
Wagura Maurice <business@waguramaurice.com>
2021-05-09 17:08:11 +0300

    Publishing the configuration & certificate files.

---
+++ config/mpesa.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    
+    /*
+        * Lipa Na MPesa credentials.
+    */
+    'environment'        => env('LNMO_ENVIRONMENT', 'sandbox'),
+    'consumer_key'       => env('LNMO_CONSUMER_KEY', 'uKxU78Y9q2cFruO2fKRWuofRCObzMQh8'),
+    'consumer_secret'    => env('LNMO_CONSUMER_SECRET', 'By9NUqT7NGhzy5Pj'),
+    'shortcode'          => env('LNMO_SHORTCODE', '174379'),
+    'key'                => env('LNMO_KEY', 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'),
+    'initiator_username' => env('LNMO_INITIATOR_USERNAME', 'testapi779'),
+    'initiator_password' => env('LNMO_INITIATOR_PASSWORD', 'HaVh3tgp'),
+    
+];
