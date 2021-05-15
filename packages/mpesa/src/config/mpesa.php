076f78e93862d136b82d70fd71dbf0ff25b9fecc
Wagura Maurice <business@waguramaurice.com>
2021-05-16 02:14:14 +0300

    b2c config added to mpesa

--- packages/mpesa/src/config/mpesa.php
+++ packages/mpesa/src/config/mpesa.php
@@ -24,12 +24,27 @@ return [
         'environment' => env('C2B_ENVIRONMENT', 'sandbox'),
         'shortcode'   => env('C2B_SHORTCODE', '600779'),
         'consumer'    => [
-            'key'    => env('LNMO_CONSUMER_KEY', 'uKxU78Y9q2cFruO2fKRWuofRCObzMQh8'),
-            'secret' => env('LNMO_CONSUMER_SECRET', 'By9NUqT7NGhzy5Pj')
+            'key'    => env('C2B_CONSUMER_KEY', 'uKxU78Y9q2cFruO2fKRWuofRCObzMQh8'),
+            'secret' => env('C2B_CONSUMER_SECRET', 'By9NUqT7NGhzy5Pj')
         ],
         'initiator'   => [
-            'username' => env('LNMO_INITIATOR_USERNAME', 'testapi779'),
-            'password' => env('LNMO_INITIATOR_PASSWORD', 'HaVh3tgp')
+            'username' => env('C2B_INITIATOR_USERNAME', 'testapi779'),
+            'password' => env('C2B_INITIATOR_PASSWORD', 'HaVh3tgp')
+        ]
+    ],
+    /*
+        * Business to Client credentials.
+    */
+    'b2c' => [
+        'environment' => env('B2C_ENVIRONMENT', 'sandbox'),
+        'shortcode'   => env('B2C_SHORTCODE', '600779'),
+        'consumer'    => [
+            'key'    => env('B2C_CONSUMER_KEY', 'uKxU78Y9q2cFruO2fKRWuofRCObzMQh8'),
+            'secret' => env('B2C_CONSUMER_SECRET', 'By9NUqT7NGhzy5Pj')
+        ],
+        'initiator'   => [
+            'username' => env('B2C_INITIATOR_USERNAME', 'testapi779'),
+            'password' => env('B2C_INITIATOR_PASSWORD', 'HaVh3tgp')
         ]
     ]
 ];
