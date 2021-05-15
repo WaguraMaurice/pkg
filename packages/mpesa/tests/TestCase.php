3e21f3c9b8efba6cc60bfd9061add0d471d916c2
Wagura Maurice <business@waguramaurice.com>
2021-05-15 12:42:29 +0300

    init new mpesa package using laravel-package-maker by Naoray

---
+++ packages/mpesa/tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Waguramaurice\Mpesa\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Waguramaurice\Mpesa\MpesaServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application   $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        //
+    }
+}
