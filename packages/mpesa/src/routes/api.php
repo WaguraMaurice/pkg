cb5c33d90353e69e388704a69a810215cd2b004b
Wagura Maurice <business@waguramaurice.com>
2021-05-15 20:00:43 +0300

    c2b url names updated to append mpesa name

--- packages/mpesa/src/routes/api.php
+++ packages/mpesa/src/routes/api.php
@@ -24,17 +24,17 @@ Route::group(['middleware' => 'api', 'prefix' => 'api/vendor/mpesa/'], function 
     Route::post('lmno/query', [LNMO_Controller::class, 'query'])->name('mpesa.lnmo.query');
 
     // MPESA C2B ROUTES
-    Route::post('C2B', [C2B_Controller::class, 'transaction'])->name('c2b.transaction');
-    Route::post('C2B/validation/callback', [C2B_Controller::class, 'validation'])->name('c2b.validation.callback');
-    Route::post('C2B/confirmation/callback', [C2B_Controller::class, 'confirmation'])->name('c2b.confirmation.callback');
-    Route::post('C2B/status', [C2B_Controller::class, 'status'])->name('c2b.status');
-    Route::post('C2B/status/callback', [C2B_Controller::class, 'statusCallback'])->name('c2b.status.callback');
-    Route::post('C2B/reverse', [C2B_Controller::class, 'reverseTransaction'])->name('c2b.reverse.transaction');
-    Route::post('C2B/reverse/callback', [C2B_Controller::class, 'reverseTransactionCallback'])->name('c2b.reverse.transaction.callback');
-    Route::get('C2B/balance', [C2B_Controller::class, 'balance'])->name('c2b.balance');
-    Route::post('C2B/balance/callback', [C2B_Controller::class, 'balanceCallback'])->name('c2b.balance.callback');
+    Route::post('C2B', [C2B_Controller::class, 'transaction'])->name('mpesa.c2b.transaction');
+    Route::post('C2B/validation/callback', [C2B_Controller::class, 'validation'])->name('mpesa.c2b.validation.callback');
+    Route::post('C2B/confirmation/callback', [C2B_Controller::class, 'confirmation'])->name('mpesa.c2b.confirmation.callback');
+    Route::post('C2B/status', [C2B_Controller::class, 'status'])->name('mpesa.c2b.status');
+    Route::post('C2B/status/callback', [C2B_Controller::class, 'statusCallback'])->name('mpesa.c2b.status.callback');
+    Route::post('C2B/reverse', [C2B_Controller::class, 'reverseTransaction'])->name('mpesa.c2b.reverse.transaction');
+    Route::post('C2B/reverse/callback', [C2B_Controller::class, 'reverseTransactionCallback'])->name('mpesa.c2b.reverse.transaction.callback');
+    Route::get('C2B/balance', [C2B_Controller::class, 'balance'])->name('mpesa.c2b.balance');
+    Route::post('C2B/balance/callback', [C2B_Controller::class, 'balanceCallback'])->name('mpesa.c2b.balance.callback');
     // use/hit only once.
-    Route::get('C2B/register', [C2B_Controller::class, 'register'])->name('c2b.register');
+    Route::get('C2B/register', [C2B_Controller::class, 'register'])->name('mpesa.c2b.register');
 
     // MPESA B2C ROUTES
     /* Route::post('B2C/transaction', [B2C_Controller::class, 'transaction'])->name('b2c.transaction');
